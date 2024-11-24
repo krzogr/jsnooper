fc93c62528962b812cad0e8d1c104df83b934204
Krzysztof <kogrodnik@yahoo.com>
2024-11-24 21:43:15 +0000

    Initial upload

--- src/main/java/org/krzogr/jsnooper/tracking/TrackingScope.java
+++ src/main/java/org/krzogr/jsnooper/tracking/TrackingScope.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 krzogr (krzogr@gmail.com)
+ * Copyright (C) 2024 krzogr (krzogr@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
